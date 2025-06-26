119a1aebd8fc951c6bb7ec49ff25b267bdf95dac
Sakey <sheikh36ahmed@gmail.com>
2025-06-26 22:24:50 +0100

    variables added to css

---
+++ script.js
@@ -0,0 +1,20 @@
+//maybe a dark/ight mode as well
+
+
+// nav bar moving underline
+const home_nav = document.querySelector("#home-nav");
+const about_nav = document.querySelector("#about-nav");
+const project_nav = document.querySelector("#project-nav");
+const educatipn_nav = document.querySelector("#education-nav");
+
+
+//show modules on hover
+
+
+
+// form 
+const in_name = document.querySelector("#input-name");
+const in_email = document.querySelector("#input-email");
+const msg_box = document.querySelector("#msg-box");
+const submit_btn = document.querySelector("#submit-btn");
+
