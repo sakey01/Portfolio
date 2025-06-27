3ababd9ed414282bd5ebfe2fe80064d10b37c707
Sakey01 <sheikh36ahmed@gmail.com>
2025-06-27 13:07:06 +0100

    js content added and fixed scroll anchor

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-//maybe a dark/ight mode as well
+//optional dark/light toggle
 
 
-// nav bar moving underline
+// moving underline for nav bar
 const home_nav = document.querySelector("#home-nav");
 const about_nav = document.querySelector("#about-nav");
 const project_nav = document.querySelector("#project-nav");
 const educatipn_nav = document.querySelector("#education-nav");
 
 
-//show modules on hover
+//show modules on hover (education section)
 
 
 
-// form 
+// form to send message
 const in_name = document.querySelector("#input-name");
 const in_email = document.querySelector("#input-email");
 const msg_box = document.querySelector("#msg-box");
 const submit_btn = document.querySelector("#submit-btn");
 
+submit_btn.addEventListener("click" , function(e){
+
+
+})
