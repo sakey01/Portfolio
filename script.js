21785fbe5910b26492a23017cccf18b1e8092e71
Sakey01 <sheikh36ahmed@gmail.com>
2025-06-30 14:30:37 +0100

    menu function added

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
 // optional dark/light toggle
 
-
 //media query to hide nav bar and have a menu instead
+
+document.addEventListener("DOMContentLoaded", () => {
+  typeWriter();
+
+  const menuIcon = document.getElementById("menu-icon");
+  const navLinks = document.querySelector(".nav-group");
+  const links = navLinks.querySelectorAll("a");
+
+  menuIcon.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+  });
+
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      if (navLinks.classList.contains("active")) {
+        navLinks.classList.remove("active");
+      }
+    });
+  });
+});
 
 // moving underline for nav bar
 const navItems = document.querySelectorAll("nav ul li");
 
-const Scrollobserver = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      console.log('Now viewing:', entry.target.id);
-    }
-  });
-}, { threshold: 0.5 });
-
+const Scrollobserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        console.log("Now viewing:", entry.target.id);
+      }
+    });
+  },
+  { threshold: 0.5 }
+);
 
 // typing quote effect
 let i = 0;
 function typeWriter(quote) {
-   quote =
+  quote =
     '"The best error message is the one that never shows up. — Thomas Fuchs"';
   if (i < quote.length) {
     document.querySelector("#home-quote").innerHTML += quote.charAt(i);
     i++;
-    setTimeout(typeWriter, 50);
+    setTimeout(typeWriter, 40);
   }
   return;
 }
-
-typeWriter();
 
 // show modules on hover (education section)
 
